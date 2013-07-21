4b799754d879467cf5a05fdc73b64fe3691332f5
Viktor Nagy <viktor.nagy@gmail.com>
2013-07-21 12:00:41 +0200

    chmod -executable

--- mailchimp/models.py
+++ mailchimp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+try:
+    from cms.models import CMSPlugin
+
+    class SignupFormPlugin(CMSPlugin):
+        list_id = models.CharField(max_length=30)
+
+        def __unicode__(self):
+            return self.list_id
+except ImportError:
+    pass
