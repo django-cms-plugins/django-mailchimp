a02d35d6d7f36e736a51d1eb3a7b1d291cd45c4d
Viktor Nagy <viktor.nagy@gmail.com>
2013-07-24 21:12:00 +0300

    fixed reference to template

--- cmsplugin_mailchimp/cms_plugins.py
+++ cmsplugin_mailchimp/cms_plugins.py
@@ -1,5 +1,7 @@
 from django.template import Context
 from django.utils.translation import ugettext as _
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
 
 from .forms import SubscribeForm
 from .models import SignupFormPlugin
@@ -7,7 +9,7 @@ from .models import SignupFormPlugin
 class MailchimpPlugin(CMSPluginBase):
     model = SignupFormPlugin
     name = _('Mailchimp Signup Form')
-    render_template = 'mailchimp/subscribe.html'
+    render_template = 'cmsplugin_mailchimp/subscribe.html'
 
     def render(self, context, instance, placeholder):
         subscribeForm = SubscribeForm(initial={'list_id': instance.list_id})
